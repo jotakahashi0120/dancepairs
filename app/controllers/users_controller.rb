7788fd3b79329b3933c77796547a8e4dbc6a02df
jotakahashi0120 <jo.takahashi.0120@gmail.com>
2020-09-22 08:13:01 +0000

    ユーザー一覧表示と詳細表示を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.order(updated_at: :desc)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 end
