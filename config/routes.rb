b831bab0c20daa18e0560ec8ef11711bfdfdccdd
jotakahashi0120 <jo.takahashi.0120@gmail.com>
2020-09-23 16:37:44 +0000

    routesを編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
   resources :users
+  resources :messages, :only => [:create]
+  resources :rooms, :only => [:create, :show, :index]
 end
